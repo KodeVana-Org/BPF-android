1ec275e6b81a358c1a4e1e3f89a2a2c50a872947
Dipen Boro <borod9200@gmail.com>
2026-07-26 14:00:43 +0530

    add an dummy api for wake up render server

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigator/RootNavigator';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {AppContextProvider} from './src/navigator/AppContext';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AppContextProvider } from './src/navigator/AppContext';
 import InternetConnectionAlert from './src/components/Toast/InternetConnectionAlert';
 import Toast from 'react-native-toast-message';
 
-import {QueryClient} from '@tanstack/react-query';
-import {PersistQueryClientProvider} from '@tanstack/react-query-persist-client';
-import {createAsyncStoragePersister} from '@tanstack/query-async-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { wakeServer } from './src/api/join-donate_apis';
 
 // ✅ Setup the QueryClient
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
       retry: 3,
       refetchOnWindowFocus: false,
     },
@@ -29,6 +30,15 @@ export const asyncStoragePersister = createAsyncStoragePersister({
 });
 
 const App = () => {
+  // for walking up the server 
+  // i am using render free server 
+  // so it required to wake up 
+  // so this is dummy api 
+  useEffect(() => {
+    // console.log("WAKING UP SERVER..");
+    wakeServer();
+  }, []);
+
   return (
     <PersistQueryClientProvider
       client={queryClient}
--- src/api/ApiManager.ts
+++ src/api/ApiManager.ts
@@ -3,8 +3,13 @@ import axios from 'axios';
 // cant use localhost here
 const ApiManager = axios.create({
   baseURL: 'https://bpf-backend.onrender.com/',
-  timeout: 15000,
-  // baseURL: 'http://10.197.91.13:6969/',
+  // timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Accept-Encoding': 'gzip, deflate', // Forces compressed fast payloads like Postman
+  },
+  // baseURL: 'http://10.55.14.13:6969/',
   // 10.197.91.13
   // responseType: 'json',
   // withCredentials: true,
